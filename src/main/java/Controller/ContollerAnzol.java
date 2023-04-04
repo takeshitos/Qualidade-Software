d0e9e93f386c4b2a000fa14a790e14fbe0e65c16
takeshitos <rictakeshi.miyamoto@gmail.com>
2023-04-04 17:35:11 -0300

    MVC

---
+++ src/main/java/Controller/ContollerAnzol.java
@@ -0,0 +1,14 @@
+package Controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ricar
+ */
+public class ContollerAnzol {
+    
+}
